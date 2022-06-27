870f5dfab9e83f577089ba0026d8d5fa1bd0dbb8
KANGDONGHOON0413 <kmerits@gmail.com>
2022-06-27 20:51:36 +0900

    초기 커밋.

---
+++ day14_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace day14_01
+{
+    class Program
+    {
+        delegate void dele(string str);
+        class A
+        {
+            public void Prints(string str)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //1번 방법
+            A p01 = new A();
+            dele D1 = new dele(p01.Prints);
+            D1("kang");
+
+            //2번 방법
+            dele D2 = p01.Prints;
+            D2("hi");
+
+        }
+    }
+}
